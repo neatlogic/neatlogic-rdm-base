28a0a396612077a4f99b8a458e309e1e0fa46c9d
陈其炜 <chenqw@techsure.com.cn>
2022-12-19 17:57:39 +0800

    init
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/framework/rdm/dto/ProjectTemplateObjectTypeVo.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.rdm.dto;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.rdm.enums.ObjectType;
+import codedriver.framework.restful.annotation.EntityField;
+import org.apache.commons.lang3.StringUtils;
+
+public class ProjectTemplateObjectTypeVo {
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    private String label;
+    @EntityField(name = "唯一标识", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "排序", type = ApiParamType.INTEGER)
+    private Integer sort;
+
+    public String getLabel() {
+        if (StringUtils.isBlank(label) && StringUtils.isNotBlank(name)) {
+            label = ObjectType.getText(name);
+        }
+        return label;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+}
